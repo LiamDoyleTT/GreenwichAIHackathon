5b189169a834be145371efdcf17e26c7c081c0d3
pankaj naug <pankaj.naug@royalgreenwich.gov.uk>
2025-06-12 16:40:00 +0100

    update response messages

--- api/api/chat/chat_handler.py
+++ api/api/chat/chat_handler.py
@@ -81,25 +81,26 @@ class ChatHandler:
         #     return SimpleNamespace(content="Goodbye! Thanks for your time.")
 
         messages = self.parse_conversation(input_text)
-        search_response = search_handler.get_query_response(messages)
+        search_response = search_handler.get_query_response(input_text)
         
         # bot for extracting the postcode
         prompt = ChatPromptTemplate.from_messages(
             [
                 (
                     "system",
-                    """You are a bot for extracting the customer postcode from previous messages in a customer service conversation with a user.
+                    """Your purpose is extracting the customer postcode from previous messages in a customer service conversation with a user.
                     The postcode format should be between 5 and 7 characters long and contain a space. e.g. "AB12 3CD".
-                    If you cannot find a postcode in the conversation, respond with exactly the following phrase "No postcode found"
-                    Once you find a postcode, respond with the postcode only.
-                    """,
-                )
-            ] + messages
+                    There are only two ways that you can respond. Do not respond with anything else except the following:
+                    1. If you cannot find a postcode in the conversation, respond with exactly the following phrase "No postcode found"
+                    2. If you can find a postcode in the conversation history, respond with the postcode only and nothing else.
+                    """), 
+                    ("human", "Extract the postcode from the conversation history following. If you cannot find a postcode, respond with 'No postcode found'. Conversation history: {input}")
+            ]
         )
         chain = prompt | self.llm
         postcode_response = chain.invoke(
             {
-                "input": input_text            }
+                "input": str(messages)          }
         )
 
         # agent for pulling out the full postcode if not yet provided
@@ -109,7 +110,7 @@ class ChatHandler:
                     (
                         "system",
                         """You are an experienced and efficient customer service agent; you are answering calls to customers about missed bin collections on behalf of the Royal Borough of Greenwich.  
-                        Your only role is deciding if the query is a missed bin query. If it is, then you need to ask questions to identify the customers' full postcode e.g. AB12 3CD, and the colour of the bin that was missed.
+                        Your only role is deciding if the query is a missed bin query and if it is, then you need to ask questions first to identify what colour bin was missed and only once this is established, then asking for the customers' full postcode e.g. AB12 3CD.
                         You do not deal with missed communal bin collection or missing bins. If the customer mentions anything other than missed bin collections, then ask them if they would like to be routed through to a specialist customer service agent.
                         When starting the conversation, give the caller a hello and welcome to Royal Greenwich and an introduction with your name, Richard.
                         If you are unclear of what the customers' request is, then ask them politely how you can help them. 
@@ -167,10 +168,11 @@ class ChatHandler:
                             break
                         else:
                             missed_reason = collections_result.get("collectionMessage")
+                            print("missed_reason:", missed_reason)
 
                 # Loop through the collections_results to check if any collection is missed
                 if isElligibleForMissedBin:
-                    response = "You are eligible for a missed " + missed_service + ". Please contact the council to arrange this."
+                    response = "I'm sorry that your bin collection has been missed. We can arrange for the bin to be collected for you. Would you like me to do this for you?"
                     return response
                 else:
                     # agent for responding to failed postcode lookup
@@ -179,9 +181,9 @@ class ChatHandler:
                             (
                                 "system",
                                     """You are an experienced and efficient customer service agent; you are answering calls to customers about missed bin collections on behalf of the Royal Borough of Greenwich.  
-                                        You are only addressing customers where the bin was missed, and we cannot book a recollection. The reason the bin was missed was because of the following: {missed_reason}.
-                                        You will need to explain this to the customer, with reference to this reason and any relevant documentation in the following: {information}.
-                                        Only if the customer sounds angry or frustrated you will be apologetic.
+                                        You are only addressing customers where the bin was missed, and we cannot book a collection. The reason the bin was missed was because of the following: {missed_reason}.
+                                        You will need to explain that the bin was missed and collection cannot be arranged. Then explain the reason for this based on the defined reason: {missed_reason} and any relevant documentation on why a bin may be missed in the following: {information}.
+                                        Only if the customer sounds angry or frustrated you will be apologetic. Do not respond with any information other than what is required.
                                         Keep responses to short sentences and only ask one question at a time and only give the caller one action per response. 
                                     """,
                             )
